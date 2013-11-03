836b0f8e0425731abf8fe19f5cedda7e1d073d1f
rybka <rybka@akwarium>
2013-11-03 15:34:22 +0100

    gotowe

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+ 	validates :title, presence: true,
+                    length: { minimum: 5 }
 
-  belongs_to :category
+  	belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Blog::Application.routes.draw do
 
-
-
+  resources :posts do
+    resources :comments
+  end
 
   resources :categories do
     resources :posts, :only => :show
